2542d7dc46e2ea5a8a02fca1d429e4773c94d9f4
HasnainArifYU <harif@pgh.yearup.org>
2024-06-10 12:21:45 -0400

    sql

---
+++ Java-Development/workbook-8/NorthwindProducts/src/main/java/com/pluralsight/NP.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+import java.sql.*;
+public class NP {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection; connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/", "root", "admin");
+
+        Statement statement = connection.createStatement();
+
+        String query = "SELECT * FROM northwind.products;";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while (results.next()) {
+            String resultsString = results.getString("ProductName");
+            System.out.println(resultsString);
+        }
+
+
+        connection.close();
+
+    }
+}
